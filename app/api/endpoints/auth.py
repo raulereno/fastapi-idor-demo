2e3b65795d43735b26bf728aad8ef8630555c4a6
raulereno <raulereno@gmail.com>
2025-07-24 00:32:23 -0300

    Update access token payload to use user ID as a string instead of username

--- app/api/endpoints/auth.py
+++ app/api/endpoints/auth.py
@@ -36,6 +36,6 @@ async def login(user_credentials: UserLogin, db: Session = Depends(get_db)):
     
     access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
+        data={"sub": str(user.id)}, expires_delta=access_token_expires
     )
     return {"access_token": access_token, "token_type": "bearer"} 
